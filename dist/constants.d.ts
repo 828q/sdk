ff94a7bbd2f322e7b76624ca99be0fb142798282
828q <orbiiverde@gmail.com>
2026-08-27 00:34:18 +0100

    Add files via upload

---
+++ dist/constants.d.ts
@@ -0,0 +1,40 @@
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | number | string;
+export declare enum ChainId {
+    MAINNET = 1380996178
+}
+export declare enum TradeType {
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
+}
+export declare enum Rounding {
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
+}
+export declare const FACTORY_ADDRESS = "0x011A979A558baDb216d4927DAb5641D2c5823636";
+export declare const FACTORY_ADDRESS_MAP: {
+    1380996178: string;
+};
+export declare const INIT_CODE_HASH = "0xb887cbb9306a113362d69a65c1d2a81d88f726005b8883fc1a0273184417fbc0";
+export declare const INIT_CODE_HASH_MAP: {
+    1380996178: string;
+};
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const FEES_NUMERATOR: JSBI;
+export declare const FEES_DENOMINATOR: JSBI;
+export declare enum SolidityType {
+    uint8 = "uint8",
+    uint256 = "uint256"
+}
+export declare const SOLIDITY_TYPE_MAXIMA: {
+    uint8: JSBI;
+    uint256: JSBI;
+};
