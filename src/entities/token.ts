fa771d4dd89a27b50e560b2343e2f651dd94cd10
828q <orbiiverde@gmail.com>
2026-08-26 23:04:28 +0000

    Initial commit

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -68,18 +68,10 @@ export function currencyEquals(currencyA: Currency, currencyB: Currency): boolea
 export const WETH = {
   [ChainId.MAINNET]: new Token(
     ChainId.MAINNET,
-    '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    '0xeF7cADE66695f4cD8a535f7916fBF659936818C4',
     18,
-    'WBNB',
-    'Wrapped BNB',
-    'https://www.binance.org'
-  ),
-  [ChainId.TESTNET]: new Token(
-    ChainId.TESTNET,
-    '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
-    18,
-    'WBNB',
-    'Wrapped BNB',
-    'https://www.binance.org'
+    'WRPTR',
+    'Wrapped Raptor',
+    'https://www.raptorchain.io'
   )
 }
